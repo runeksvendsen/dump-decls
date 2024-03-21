59401abf0fc774eb7df9c621efd91cb1b64680e7
Rune K. Svendsen <runesvend@gmail.com>
2024-03-21 11:46:29 +0100

    build-hls.sh.nix: append GHC version to HLS filename

--- build-hls.nix
+++ build-hls.nix
@@ -14,6 +14,7 @@ pkgs.writeScriptBin "build-hls.sh" ''
     set -e
 
     EXE_DIR="${exe-dir-expression}"
+    GHC_VERSION=$(ghc --numeric-version)
 
     cd "$(mktemp -d)"
     git clone https://github.com/haskell/haskell-language-server.git
@@ -22,5 +23,6 @@ pkgs.writeScriptBin "build-hls.sh" ''
     cabal build exe:haskell-language-server
     cabal install --installdir "$EXE_DIR" --overwrite-policy=always exe:haskell-language-server
     echo "Built haskell-language-server v${hls-version}"
+    mv "$EXE_DIR/haskell-language-server" "$EXE_DIR/haskell-language-server-$GHC_VERSION"
     echo "Executables installed in $(realpath $EXE_DIR)"
 ''
