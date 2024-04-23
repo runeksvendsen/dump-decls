9c010e009ad4a8405b490b1d9a4a42f1dce52681
Rune K. Svendsen <runesvend@gmail.com>
2024-04-23 13:21:50 +0200

    Support GHC 9.8 (#3)

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -4,6 +4,12 @@
       sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
     }) {};
 
+  pkgs98 =
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/91a91b9ae468de8500dccb74c7896b1218e9c1de.tar.gz";
+      sha256 = "1knklrnvd9syyc48vmbjj7q5il75i4k928qq2c2d7g3jm03k9aya";
+    }) {};
+
   pkgsUnstable =
     import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/6652baf493bb1092f9d7db1c0e29d493f10c9f16.tar.gz";
