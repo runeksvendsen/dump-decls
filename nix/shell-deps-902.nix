19eb9d92bf268e8761baaeef9f6b464e5c3d9332
Rune K. Svendsen <runesvend@gmail.com>
2024-04-23 13:25:09 +0200

    nix-shell: add HLS

--- nix/shell-deps-902.nix
+++ nix/shell-deps-902.nix
@@ -1,6 +1,7 @@
 with (import ./pkgs.nix);
 { nativeBuildInputs = [
     pkgs.haskell.compiler.ghc90
+    pkgs.haskell.packages.ghc90.haskell-language-server
     pkgsUnstable.cabal-install
     pkgs.git
   ];
--- nix/shell-deps.nix
+++ nix/shell-deps.nix
@@ -1,6 +1,7 @@
 with (import ./pkgs.nix);
 { nativeBuildInputs = [
     pkgs.haskell.compiler.ghc96
+    pkgs.haskell.packages.ghc96.haskell-language-server
     pkgsUnstable.cabal-install
     pkgs.git
     pkgs.zlib # needed for building haskell-language-server
