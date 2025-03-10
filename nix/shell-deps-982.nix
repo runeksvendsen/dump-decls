2512fc9bae365526a75189395046887dd719490f
Rune K. Svendsen <runesvend@gmail.com>
2025-03-10 10:55:57 +0100

    Revert unrelated changes

--- nix/shell-deps-982.nix
+++
@@ -1,8 +0,0 @@
-with (import ./pkgs.nix);
-{ nativeBuildInputs = [
-    pkgs98.haskell.compiler.ghc98
-    pkgsUnstable.cabal-install
-    pkgs.git
-    pkgs.zlib # needed for building haskell-language-server
-  ];
-}
